1d7dbe2b89ee3a092b62d2af6a2e13348c20b695
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-05-03 03:57:54 -0400

    Update shader.cc

--- src/gpgpu-sim/shader.cc
+++ src/gpgpu-sim/shader.cc
@@ -2096,7 +2096,8 @@ bool ldst_unit::memory_cycle(warp_inst_t &inst,
       bypassL1D = true;
   }
  if(profiling==1)
- { my_map[m_sid][kerne_id_temp][address]; //counter}
+ { my_map[m_sid][kerne_id_temp][address]; //counter
+ }
     //int k=0;
  else
  {

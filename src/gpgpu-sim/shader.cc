c53e87623714f8449685432abe45df1d03737b57
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-05-03 12:08:28 -0400

    Update shader.cc

--- src/gpgpu-sim/shader.cc
+++ src/gpgpu-sim/shader.cc
@@ -2658,9 +2658,10 @@ void ldst_unit::cycle() {
    unsigned address=mf->get_addr();// address ID
   
   address=address >>7;
-          //if(profiling==1)
- // my_map[m_sid][kerne_id_temp][address]++; //counter
-              //int k=0;
+         if(profiling==0){
+ if(my_map[m_sid][kerne_id_temp][address]<3) //counter
+              {bypassL1D=true;}//int k=0;
+         }
  // else
  // {
     //if(my_map[m_sid][kerne_id_temp][address] < 3)
